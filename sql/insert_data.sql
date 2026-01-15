e30f178b9a536eaee782446ccf12f1d2a13bff82
qxPepper <qxpepper@mail.ru>
2026-01-15 14:17:15 +0300

    feat: add 7 more car records for analysis

--- sql/insert_data.sql
+++ sql/insert_data.sql
@@ -2,3 +2,12 @@ INSERT INTO cars (brand, model, year, color, price) VALUES
 ('Toyota', 'Camry', 2020, 'Silver', 1500000.00),
 ('BMW', 'X5', 2022, 'Black', 4500000.50),
 ('Lada', 'Vesta', 2021, 'White', 800000.00);
+
+INSERT INTO cars (brand, model, year, color, price) VALUES
+('Ford', 'Focus', 2015, 'Blue', 550000.00),
+('Hyundai', 'Getz', 2019, 'White', 700000.00),
+('Kia', 'Sportage', 2021, 'Black', 2100000.00),
+('Mercedes', 'ML', 2022, 'White', 4800000.00),
+('Audi', 'A6', 2016, 'Grey', 870000.00),
+('Volkswagen', 'Polo', 2021, 'White', 2000000.00),
+('Nissan', 'X-Trail', 2024, 'Silver', 3700000.00);
